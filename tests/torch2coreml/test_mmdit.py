95208366942793fa4b44105ab5024e3a62e5c151
Arda Okan <151988617+arda-argmax@users.noreply.github.com>
2024-06-14 08:10:45 -0700

    convert to coreml from local ckpt (#2)

--- tests/torch2coreml/test_mmdit.py
+++ tests/torch2coreml/test_mmdit.py
@@ -47,6 +47,7 @@ class TestSD3MMDiT(argmaxtools_test_utils.CoreMLTestsMixin, unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        global TEST_SD3_CKPT_PATH
         cls.model_name = "MultiModalDiffusionTransformer"
         cls.test_output_names = ["denoiser_output"]
         cls.test_cache_dir = TEST_CACHE_DIR
@@ -60,7 +61,9 @@ class TestSD3MMDiT(argmaxtools_test_utils.CoreMLTestsMixin, unittest.TestCase):
             .eval()
         )
         logger.info("Initialized.")
-        TEST_SD3_CKPT_PATH = hf_hub_download(TEST_SD3_HF_REPO, "sd3_medium.safetensors")
+        TEST_SD3_CKPT_PATH = TEST_SD3_CKPT_PATH or hf_hub_download(
+            TEST_SD3_HF_REPO, "sd3_medium.safetensors"
+        )
         if TEST_SD3_CKPT_PATH is not None:
 
             logger.info(f"Loading SD3 model checkpoint from {TEST_SD3_CKPT_PATH}")
@@ -133,6 +136,9 @@ if __name__ == "__main__":
     parser.add_argument("--latent-size", default=TEST_LATENT_SIZE, type=int)
     args = parser.parse_args()
 
+    TEST_SD3_CKPT_PATH = (
+        args.sd3_ckpt_path if os.path.exists(args.sd3_ckpt_path) else None
+    )
     TEST_SD3_HF_REPO = args.sd3_ckpt_path
     TEST_LATENT_SIZE = args.latent_size
     TEST_CKPT_FILE_NAME = args.ckpt_file_name
--- tests/torch2coreml/test_vae.py
+++ tests/torch2coreml/test_vae.py
@@ -45,6 +45,7 @@ class TestSD3VAEDecoder(argmaxtools_test_utils.CoreMLTestsMixin, unittest.TestCa
 
     @classmethod
     def setUpClass(cls):
+        global TEST_SD3_CKPT_PATH
         cls.model_name = "VAEDecoder"
         cls.test_output_names = ["image"]
         cls.test_cache_dir = TEST_CACHE_DIR
@@ -56,7 +57,9 @@ class TestSD3VAEDecoder(argmaxtools_test_utils.CoreMLTestsMixin, unittest.TestCa
         )
         logger.info("Initialized.")
 
-        TEST_SD3_CKPT_PATH = hf_hub_download(TEST_SD3_HF_REPO, "sd3_medium.safetensors")
+        TEST_SD3_CKPT_PATH = TEST_SD3_CKPT_PATH or hf_hub_download(
+            TEST_SD3_HF_REPO, "sd3_medium.safetensors"
+        )
         if TEST_SD3_CKPT_PATH is not None:
             logger.info(f"Loading SD3 model checkpoint from {TEST_SD3_CKPT_PATH}")
             _load_vae_decoder_weights(cls.test_torch_model, TEST_SD3_CKPT_PATH)
@@ -103,6 +106,9 @@ if __name__ == "__main__":
     parser.add_argument("--latent-size", default=TEST_LATENT_SIZE, type=int)
     args = parser.parse_args()
 
+    TEST_SD3_CKPT_PATH = (
+        args.sd3_ckpt_path if os.path.exists(args.sd3_ckpt_path) else None
+    )
     TEST_SD3_HF_REPO = args.sd3_ckpt_path
     TEST_LATENT_SIZE = args.latent_size
 
