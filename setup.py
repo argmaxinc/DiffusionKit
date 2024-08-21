ccf650985cb17df0395c3bee53047828e0fdd17c
atila <atiorh@icloud.com>
2024-08-21 08:37:35 -0700

    Increment patch version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 
 
 class VersionInstallCommand(install):
