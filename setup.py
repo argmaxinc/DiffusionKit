ad0ad240895d000c7131d753ea7ac60dd424aa12
arda-argmax <arda@takeargmax.com>
2024-08-27 01:14:39 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 
 
 class VersionInstallCommand(install):
