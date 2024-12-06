45e11445042828a2328292b54877c102cb90cee8
arda-argmax <arda@takeargmax.com>
2024-12-06 14:03:36 -0800

    bump up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 
 
 class VersionInstallCommand(install):
